e16763ac931fd34a8fd1b63a22928caecd387469
DanielY1108 <scarlet040@gmail.com>
2023-03-02 15:57:12 +0900

    Observable의 개념 및 생성 제거 방법 정리

---
+++ PlayGround/Observable-개념.playground/Contents.swift
@@ -0,0 +1,96 @@
+import UIKit
+import RxSwift
+
+// MARK: - 기본 옵저버블 생성 및 구독 방법
+
+let observable = Observable.of(1, 2, 3, 4)
+
+// 이렇게만 해주면 구독은 하지만 구독한 데이터를 사용을 하지 않는다.
+observable.subscribe()
+
+// 구독시 Observable이 방출하는 이벤트가 존재한다. (onNext, onError, onCompleted, onDisposed)
+// 이와같이 Observable은 시간의 흐름에 따라 이벤트를 방출해준다.
+// 방법. 1
+observable.subscribe { element in
+    print("Observable로 부터 \(element)를 전달 받았습니다.")
+} onError: { err in
+    print(err.localizedDescription)
+} onCompleted: {
+    print("Observable이 정상적으로 종료 되었습니다.")
+} onDisposed: {
+    print("Observable이 버려졌습니다.")
+}
+
+// Observable로 부터 1를 전달 받았습니다.
+// Observable로 부터 2를 전달 받았습니다.
+// Observable로 부터 3를 전달 받았습니다.
+// Observable로 부터 4를 전달 받았습니다.
+// Observable이 정상적으로 종료 되었습니다.
+// Observable이 버려졌습니다.
+
+// Observable의 이벤트 처리는 subscribe을 통해 시간의 흐름에 따라 이벤트를 처리를 시킨다.
+
+
+// MARK: - subscribe 참고
+
+// 구독(subsribe)은 한번의 호출로 한번의 이벤트만 처리한다.
+// 종료되고 사용하려면 다시 구독(subsribe)을 해줘야 한다.
+
+// subscribe의 파라미터들은 옵셔널 값이라서 생략이 가능합니다. (즉, 원하는 동작만 사용할 수 있다)
+// observable.subscribe(onNext: <#T##((Int) -> Void)?##((Int) -> Void)?##(Int) -> Void#>, onError: <#T##((Error) -> Void)?##((Error) -> Void)?##(Error) -> Void#>, onCompleted: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDisposed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+
+// 참고사항은 이와같이 파라미터를 모두 지우고 사용하게 되면 onNext와 onCompleted, onError이 모두 호출됩니다.
+observable.subscribe { element in
+    print(element)
+}
+
+// next(1)
+// next(2)
+// next(3)
+// next(4)
+// completed
+
+
+
+// 방법. 2
+observable.subscribe { event in
+    switch event {
+    case .next(let element):
+        print("Observable로 부터 \(element)를 전달 받았습니다.")
+    case .completed:
+        print("Observable이 정상적으로 종료 되었습니다.")
+    case .error(let err):
+        print(err.localizedDescription)
+    }
+}
+
+// MARK: - 구독시 메모리 누수문제 해결방법
+
+// subscribe은 escape closure이므로 self를 사용하게 되면 누수가 발생할 확률이 있다.
+// 기본적으로는 이와같이 weak self를 사용하여 누수를 해결한다.
+class Class {
+    func someFunc() {}
+    
+    init() {
+        observable.subscribe { [weak self] _ in
+            guard let self = self else { return }
+            self.someFunc()
+        }
+    }
+}
+
+// RxSwift에서는 메모리 누수를 해결하기 위해 따로 이니셜라이저를 만들어놨습니다.
+// with 파라미터가 있는 메서드를 사용하면 된다. subscribe(with:)
+// with에 self를 넘겨주고 strongSelf로 받아와서 클로저에서 사용하여 메모리 누수를 방지해줍니다.
+class ClassRx {
+    func someFunc() {}
+    
+    init() {
+        observable.subscribe(with: self) { strongSelf, _ in
+            strongSelf.someFunc()
+        }
+    }
+}
+
+
+// 즉, Observable는 이벤트를 감지하고 구독자(Observer)는 구독(subscribe)를 통해 이벤트를 처리합니다.
---
+++ PlayGround/Observable-생성 및 제거.playground/Contents.swift
@@ -0,0 +1,190 @@
+import UIKit
+import RxSwift
+
+// 생성 방법으로는 create, just, of, from, range 가 존재합니다.
+// 제거 방법으로는 empty, never, disposable이 존재합니다.
+
+// MARK: - just
+// 오직 하나의 Observable sequence만을 생성
+let justObservable = Observable.just("Hello World")
+
+
+justObservable.subscribe(onNext: { element in
+    print(element)
+})
+
+// Hello World
+
+// MARK: - of
+// of 안쪽의 타입은 동일해야 한다.
+// element을 순차적으로 방출시킨다. (방출 이벤트: element -> array)
+let ofObservable: Observable<Int> = Observable.of(1, 2, 3)
+let ofObservableArr: Observable<[Int]> = Observable.of([1, 2, 3])
+
+ofObservable.subscribe(onNext: { element in
+    print(element)
+})
+
+// 1
+// 2
+// 3
+
+// 배열 자체를 한개의 element로 취급해서 방출시킨다.
+ofObservableArr.subscribe(onNext: { element in
+    print(element)
+})
+
+// [1, 2, 3]
+
+// MARK: - from
+// 배열로 element를 받아와서 하나하나를 방출시킨다. (방출 이벤트: array -> element)
+let fromObservable = Observable.from([1, 2, 3])
+
+fromObservable.subscribe(onNext: { element in
+    print(element)
+})
+
+// 1
+// 2
+// 3
+
+// MARK: - range
+// 범위를 지정해서 카운트를 배출시킨다.
+let rangeObservable = Observable.range(start: 1, count: 5)
+
+rangeObservable.subscribe(onNext: { element in
+    print(element)
+})
+
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// from으로 range를 표현할 수 있다. (방식의 차이, 하지만 의미를 명확하게 사용하기 위해선 range를 사용하자)
+let fromRangeObservable = Observable.from(1...5)
+
+fromRangeObservable.subscribe(onNext: { element in
+    print(element)
+})
+
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+// MARK: - empty
+// 초기화 하고 싶을 때 사용. 빈 Observable을 만든다.
+// 의도적으로 Observable의 구독에서 completed 되는 순간 element를 없게 만드는 방법 등 유용하게 사용된다.
+
+let emptyObservable = Observable<Any>.empty()
+
+// MARK: - never
+// Observable이 아무런 이벤트도 방출 시키지 않도록 한다. (단! onDisposed 제외)
+
+let neverObservable = Observable<Any>.never()
+
+// MARK: - Disposable
+// Observable을 구독(subscribe)하게 되면 Disposable(일회성 리소스)을 리턴 값으로 반환합니다.
+// Disposable 사용 후 버리는 리소스입니다. (일회성 리소스)
+// 일회용이기 때문에 구독 이벤트인 onError, onCompleted, onDisposed가 호출되어 더 이상 사용되지 않을 때, 메모리 정리를 위해서 dispose()를 호출 해준다.
+// dispose는 간단히 구독 취소라고 생각하면 됩니다.
+// dispose는 Observable을 구독(subsribe)할 때 필수적으로 사용해줘야 합니다. (메모리 관리를 위해)
+
+// 위의 방식대로 해주면 이러한 에러가 발생합니다. (현재 플레이그라운드라서 표시가 안됨)
+// Result of call to 'subscribe(onNext:onError:onCompleted:onDisposed:)' is unused
+// subscribe의 리턴값을 보면 Disposable을 리턴해줍니다. Disposable에 대한 처리를 하지 않아서 경고가 생기는 거죠.
+
+// 이렇게 직접 dispose를 호출해 구독 취소가 가능하다.
+justObservable.subscribe(onNext: { element in
+    print(element)
+}).dispose()
+
+// 하지만 만약 구독이 여러개가 있다고 가정해보자.
+let subscribe1 = justObservable.subscribe { _ in }
+let subscribe2 = justObservable.subscribe { _ in }
+let subscribe3 = justObservable.subscribe { _ in }
+
+// 일일히 각각의 해제 시점을 고려해 시퀀스를 끊어줘야 한다.
+subscribe1.dispose()
+subscribe2.dispose()
+subscribe3.dispose()
+
+// 여기서 불편함을 느껴 Disposable을 주머니에 담아서 한번에 해제 시키자 생각해 만들어진게 disposeBag이다.
+
+// 생성방법은 간단합니다.
+// 전역변수로 disposeBag을 만들어 준 뒤 disposed(by:) 메서드에 Disposable를 담아주기만 하면 됩니다.
+
+// disposed(by:)의 정의는 이와같습니다. bag에 Disposable 자신을 넣어줍니다.
+// extension Disposable {
+//     public func disposed(by bag: DisposeBag) {
+//         bag.insert(self)
+//     }
+// }
+
+let disposeBag = DisposeBag()
+subscribe1.disposed(by: disposeBag)
+subscribe2.disposed(by: disposeBag)
+
+// 어? 그러면 Disposable를 disposeBag에 담아주기만하고 해제는 안시켰는데라고 생각이 드실겁니다.
+// disposeBag의 해제 시점은
+// 기본적으로 사용되고 있는 컨트롤러가 deinit 됬을 때 자동으로 dispose를 시켜줍니다.
+// 다른 방법으로는 객체 disposeBag에 임의로 nil 값을 할당했을 때 dispose가 호출 됩니다. (대신 선언할 때 옵셔널도 선언해줘야 겠죠.)
+// disposeBag = nil
+
+// disposeBag 동작방식은 dispose가 되면 for-in문을 통해 기존의 Disposable을 새로운 Disposable로 변환시켜 줍니다.
+// 간단해서 자세한건 정의에 한번 들어가서 살펴보시면 이해되실 겁니다.
+
+// MARK: - create
+// 가장 많이 사용하는 생성 방법으로 사용가자 직접 커스텀 Observable을 생성합니다.
+// 각 이벤트(onNext, onError, onCompleted, onDisposed)를 직접 escape closure를 통해 정의를 해줍니다.
+// 바로 살펴봅시다.
+
+// Observable<T>.create()로 생성합니다.
+
+// 기본적인 틀은 이런 모양입니다.
+// 위에서 구독(subsribe)를 하게되면 Disposable(일회성 리소스)를 리턴한다고 했습니다.
+// 커스텀으로 create를 생성 시 일회성 리소스인 Disposable을 사용하기 위해 Disposables.create()로 필수적으로 구현해줘야 합니다.
+
+ let createObservable0 = Observable<String>.create { observer in
+     return Disposables.create()
+ }
+
+// 이제 이벤트에 대한 처리를 해보겠습니다.
+// 클로저의 파라미터에서 Dot(.) 접근한 뒤 onNext(), onCompleted(), onError() 데이터를 전달해주면 됩니다.
+let createObservable = Observable<String>.create { observer in
+    observer.onNext("Hello World")
+    observer.onCompleted()
+    observer.onError(NSError(domain: "GG", code: 404))
+    return Disposables.create()
+}
+
+// 생성을 했으니 구독을 해보면
+createObservable.subscribe { element in
+    print(element)
+} onCompleted: {
+    print("Complete")
+}.disposed(by: disposeBag)
+
+// Hello World
+// Complete
+
+// 기본적인 Rx 생성방법을 알아보았습니다.
+// 간단히 정리 및 사용이유를 알아봅시다
+
+// 간단히 비유를 통해 알아보자 NETFLIX라고 예를들면
+// Observable: NETFLIX의 영화나 드라마. 영상이 주기적으로 업데이트가 된다.
+// Observer: NETFLIX를 시청하는 사람
+// subscribe: NETFLIX 구독권, 구독권만 갖고 있다면 NETFLIX의 영화나 드라마가 업데이트가 되면 업데이트된 영상을 시청할 수 있다.
+
+// 이제 사용이유를 알아보면
+// Rx는 반응형 프로그래밍을 구현하기 위해 사용합니다.
+// Observable에서 이벤트가 업데이트 되면 구독자(Oberserver)는 구독(subscribe)을 통해 이벤트를 처리합니다.
+
+// 즉, 여기서 Observable의 escape closure로 데이터를 받아와서 데이터의 변화를 감지하고 구독(subscribe)을 통해 Observer에게 업데이트 된 데이터를 전달합니다.
+// 이러한 과정으로 이벤트가 전달되면 즉시 그 이벤트를 Observer가 구독(subscribe)을 통해 전달 받아 처리하여 UI 업데이트 및 데이터를 실시간으로 반영할 수 있게되는 겁니다.
+
+
